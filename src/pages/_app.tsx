f74ae90ed56bd8e62a6cb26814c1eace702251bd
mayone-du <cocomayo1201@icloud.com>
2021-02-20 00:01:39 +0900

    tailwind ok

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles/globalResets.css";
 import "../styles/globals.css";
-// import "tailwindcss/tailwind.css";
 
 const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
