acd7c31c23628e703f7c6de632de4d06f288ab78
mayone-du <cocomayo1201@icloud.com>
2021-02-25 18:51:34 +0900

    create privacy-policy page

---
+++ src/pages/privacy-policy.tsx
@@ -0,0 +1,13 @@
+import { Layout } from "../components/shared/layout/Layout";
+
+const PrivacyPolicy: React.FC = () => {
+  return (
+    <>
+      <Layout metaTitle="privacy policy" metaDescription="desc">
+        <h1>PrivacyPolicy</h1>
+      </Layout>
+    </>
+  );
+};
+
+export default PrivacyPolicy;
