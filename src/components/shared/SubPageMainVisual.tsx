e9b2328d0cf5f9425a23c0f446a782fce3440a8d
mayone-du <cocomayo1201@icloud.com>
2021-03-21 17:01:21 +0900

    update subpagemainvisual

--- src/components/shared/SubPageMainVisual.tsx
+++ src/components/shared/SubPageMainVisual.tsx
@@ -5,7 +5,7 @@ const SubPageMainVisual: React.VFC<{ title: string; subText: string }> = ({
   return (
     <>
       <article className="bg-gray-200">
-        <section className="md:py-40 pt-44 pb-24 flex flex-col items-center justify-center">
+        <section className="md:py-40 py-24 flex flex-col items-center justify-center">
           <h1 className="md:text-6xl text-3xl border-b-4 border-black">{title}</h1>
           <p className="md:py-4 py-2">{subText}</p>
         </section>
