54d98c6cb958af12d56b9639bcbb10fb16c6bc9d
mayone-du <cocomayo1201@icloud.com>
2021-03-21 21:05:25 +0900

    update header

--- src/components/shared/layout/Header.tsx
+++ src/components/shared/layout/Header.tsx
@@ -21,35 +21,35 @@ const Header: React.VFC<{ isActivePage: string }> = ({ isActivePage }) => {
           <ul className="flex items-center justify-center">
             <li>
               <Link href="/">
-                <a className={`block hover:bg-gray-100 transform transition border-black md:p-4 p-1 md:m-4 m-2 text-xs md:text-base ${
+                <a className={`block hover:bg-gray-100 transform font-custom-fontFamily font-bold text-custom-darkgray transition border-custom-darkgray md:p-4 p-1 md:mx-4 my-2 mx-1 text-base md:text-xl ${
                   isActivePage === "Home" ? "border-b-2 border-black" : ""
                 }`}>HOME</a>
               </Link>
             </li>
             <li>
               <Link href="/about">
-                <a className={`block hover:bg-gray-100 transform transition border-black md:p-4 p-1 md:m-4 m-2 text-xs md:text-base ${
+                <a className={`block hover:bg-gray-100 transform font-custom-fontFamily font-bold text-custom-darkgray transition border-custom-darkgray md:p-4 p-1 md:mx-4 my-2 mx-1 text-base md:text-xl ${
                   isActivePage === "About" ? "border-b-2 border-black" : ""
                 }`}>ABOUT</a>
               </Link>
             </li>
             <li>
               <Link href="/service">
-                <a className={`block hover:bg-gray-100 transform transition border-black md:p-4 p-1 md:m-4 m-2 text-xs md:text-base ${
+                <a className={`block hover:bg-gray-100 transform font-custom-fontFamily font-bold text-custom-darkgray transition border-custom-darkgray md:p-4 p-1 md:mx-4 my-2 mx-1 text-base md:text-xl ${
                   isActivePage === "Service" ? "border-b-2 border-black" : ""
                 }`}>SERVICE</a>
               </Link>
             </li>
             <li>
               <Link href="/works">
-                <a className={`block hover:bg-gray-100 transform transition border-black md:p-4 p-1 md:m-4 m-2 text-xs md:text-base ${
+                <a className={`block hover:bg-gray-100 transform font-custom-fontFamily font-bold text-custom-darkgray transition border-custom-darkgray md:p-4 p-1 md:mx-4 my-2 mx-1 text-base md:text-xl ${
                   isActivePage === "Works" ? "border-b-2 border-black" : ""
                 }`}>WORKS</a>
               </Link>
             </li>
             <li>
               <Link href="/contact">
-                <a className={`block hover:bg-gray-100 transform transition border-black md:p-4 p-1 md:m-4 m-2 text-xs md:text-base ${
+                <a className={`block hover:bg-gray-100 transform font-custom-fontFamily font-bold text-custom-darkgray transition border-custom-darkgray md:p-4 p-1 md:mx-4 my-2 mx-1 text-base md:text-xl ${
                   isActivePage === "Contact" ? "border-b-2 border-black" : ""
                 }`}>CONTACT</a>
               </Link>
