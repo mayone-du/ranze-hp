8b147bb5f5ee8dc68c0d150283f27aeb03d5e865
mayone-du <cocomayo1201@icloud.com>
2021-03-26 14:50:37 +0900

    update faq,sitemap

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,9 +1,27 @@
+import { Layout } from "../components/shared/layout/Layout";
+import { SubPageMainVisual } from "../components/shared/SubPageMainVisual";
+import { Block } from "../components/shared/generalBlock/Block";
+import { Row } from "../components/shared/generalBlock/Row";
+import { Column } from "../components/shared/generalBlock/Column";
+
 const FAQ: React.VFC = () => {
+  const siteTitle = "FAQ"
   return (
     <>
-      <div>
-
-      </div>
+      <Layout
+        metaTitle={`Ranze official site | ${siteTitle}`}
+        metaDescription="meta description"
+        isActivePage={siteTitle}
+      >
+        <SubPageMainVisual title="FAQ" subText="よくある質問" />
+        <Block>
+          <Row>
+            <Column>
+              <h3>hoge</h3>
+            </Column>
+          </Row>
+        </Block>
+      </Layout>
     </>
   );
 };
--- src/pages/sitemap.tsx
+++ src/pages/sitemap.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import { Layout } from "../components/shared/layout/Layout";
 import { SubPageMainVisual } from "../components/shared/SubPageMainVisual";
+import { Block } from "../components/shared/generalBlock/Block";
+import { Row } from "../components/shared/generalBlock/Row";
+import { Column } from "../components/shared/generalBlock/Column";
 
 const SiteMap: React.VFC = () => {
   const siteTitle = "SiteMap";
@@ -13,19 +16,84 @@ const SiteMap: React.VFC = () => {
       >
         <SubPageMainVisual title={siteTitle} subText="subtext" />
 
-        <article className="container mx-auto">
+        <Block
+          sectionSubText="subtext"
+          sectionTitle="title"
+        >
+          <Row>
+            <Column className="border-r-2 border-l-2">
+              <h3>LINE</h3>
+              <ul>
+                <li>
+                  <Link href="/">
+                    <a className="text-blue-500">LINEスタンプ</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/">
+                    <a className="text-blue-500">LINE絵文字</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/">
+                    <a className="text-blue-500">イラスト制作</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/">
+                    <a className="text-blue-500">アイコン制作</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/">
+                    <a className="text-blue-500">名刺制作</a>
+                  </Link>
+                </li>
+              </ul>
+            </Column>
+            <Column className="border-r-2 border-l-2">
+              <h3>LINE</h3>
+              <ul>
+                <li>
+                  <Link href="/">
+                    <a className="text-blue-500">LINEスタンプ</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/">
+                    <a className="text-blue-500">LINE絵文字</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/">
+                    <a className="text-blue-500">イラスト制作</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/">
+                    <a className="text-blue-500">アイコン制作</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/">
+                    <a className="text-blue-500">名刺制作</a>
+                  </Link>
+                </li>
+              </ul>
+            </Column>
+          </Row>
+        </Block>
+
+        {/* <article className="container mx-auto">
           <section>
             <div className="flex w-11/12 mx-auto">
               <div className="mx-4 w-full border-l-2 border-r-2 border-collapse border-gray-400">
-                <div className="flex items-center mx-auto">
-
-                </div>
+                <div className="flex items-center mx-auto"></div>
               </div>
             </div>
           </section>
-        </article>
-
-
+        </article> */}
+{/* 
         <article className="container mx-auto">
           <section>
             <div className="flex w-11/12 mx-auto">
@@ -91,8 +159,7 @@ const SiteMap: React.VFC = () => {
               </div>
             </div>
           </section>
-        </article>
-
+        </article> */}
       </Layout>
     </>
   );
