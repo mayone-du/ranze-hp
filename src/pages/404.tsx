67030723184e41591be5ef0bff4fcaa4a4ca9bfb
mayone-du <cocomayo1201@icloud.com>
2021-06-16 19:20:33 +0900

    Edit 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,15 @@
 import { Layout } from "../components/shared/layout/Layout";
-import { My404 } from "../components/shared/My404";
 
 const Custom404: React.FC = () => {
   return (
     <>
       <Layout metaTitle="404" metaDescription="not found" isActivePage="404">
-        <h1 className="pt-40">404 Not Found</h1>
-        <p>Sorry :）</p>
-        <My404 />
+        <h1 className="py-40 text-8xl text-custom-darkgray font-bold text-center">
+          404 Not Found
+        </h1>
+        <p className="py-4 text-center text-custom-darkgray">
+          Sorry :） this page is not found . . .
+        </p>
       </Layout>
     </>
   );
