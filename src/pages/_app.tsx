d8d88ba5fdd654b5656ce937dd571e26e4a4ab2d
mayone-du <cocomayo1201@icloud.com>
2021-09-29 23:07:38 +0900

    Update packages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globalResets.css";
 import "../styles/globals.css";
 
+// TODO: NextSeoの設定
 const App: React.FC<{ Component; pageProps }> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
